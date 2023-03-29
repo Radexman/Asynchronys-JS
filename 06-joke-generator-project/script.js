8341826128cc0b554b9ee45824255469edc89cbe
Radosław Siek <Borderlandsmaniak@gmail.com>
2023-03-29 20:52:39 +0200

    Jokes are working

--- 06-joke-generator-project/script.js
+++ 06-joke-generator-project/script.js
@@ -1,19 +1,23 @@
-const buttonEl = document.querySelector('.button');
-const jokeContent = document.querySelector('.joke-content');
+const jokeEl = document.querySelector('.joke-content');
+const jokeBtn = document.querySelector('.button');
 
 function generateJoke() {
 	const xhr = new XMLHttpRequest();
+
 	xhr.open('GET', 'https://api.chucknorris.io/jokes/random');
+
 	xhr.onreadystatechange = function () {
-		if (this.readyState === 4 && this.readyState === 200) {
-			const data = JSON.parse(this.responseText).value;
-			jokeContent.textContent = data;
-		} else {
-			jokeContent.textContent = 'Something went wrong (Not Funny)';
+		if (this.readyState === 4) {
+			if (this.status === 200) {
+				jokeEl.innerHTML = JSON.parse(this.responseText).value;
+			} else {
+				jokeEl.innerHTML = 'Something went wrong (Not Funny)';
+			}
 		}
 	};
 
 	xhr.send();
 }
 
-buttonEl.addEventListener('click', generateJoke);
+jokeBtn.addEventListener('click', generateJoke);
+document.addEventListener('DOMContentLoaded', generateJoke);
