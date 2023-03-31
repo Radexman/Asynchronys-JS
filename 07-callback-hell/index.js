8f4c7e0da4fc4b39a577c2a906af4b2ebc282181
Radosław Siek <Borderlandsmaniak@gmail.com>
2023-03-31 10:58:48 +0200

    31.03

--- 07-callback-hell/index.js
+++ 07-callback-hell/index.js
@@ -1,1 +1,18 @@
 //  ================== 07 Callback Hell ==================  //
+function getData(endpoint) {
+	const xhr = new XMLHttpRequest();
+
+	xhr.open('GET', endpoint);
+
+	xhr.onreadystatechange = function () {
+		if (this.readyState === 4 && this.status === 200) {
+			console.log(JSON.parse(this.responseText));
+		}
+	};
+
+	setTimeout(() => {
+		xhr.send();
+	}, Math.floor(Math.random() * 3000) + 1000);
+}
+
+getData('./movies.json');
