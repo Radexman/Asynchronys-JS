ca1bb3e3cf645a1e1e17081905dc1a1d1f2036a0
Radosław Siek <Borderlandsmaniak@gmail.com>
2023-04-02 11:07:54 +0200

    02.04

--- 08-promises/index.js
+++ 08-promises/index.js
@@ -1,1 +1,25 @@
 // ===================== 09 Promises ====================== //
+
+// Create a promise
+const promise = new Promise((resolve, reject) => {
+	setTimeout(() => {
+		let error = false;
+
+		if (!error) {
+			resolve({ name: 'Jonh', age: 30 });
+		} else {
+			reject('Error: Something went wrong');
+		}
+	}, 1000);
+})
+	.then((user) => {
+		console.log(user);
+	})
+	.catch((error) => {
+		console.log(error);
+	})
+	.finally(() => {
+		console.log('The promise has ben resolved or rejected');
+	});
+
+console.log('Hello from global scope');
