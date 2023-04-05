d32f65ead0b2cd30c2060f629cff9f8cd047bc44
Radosław Siek <Borderlandsmaniak@gmail.com>
2023-04-05 14:02:37 +0200

    05.04

--- 02-setInterval-&-clearInterval-functions/index.js
+++ 02-setInterval-&-clearInterval-functions/index.js
@@ -1,31 +1,38 @@
-// ========= setInterval and clearInterval Functions ========== //
+// ======== setInterval and clearInterval Functions ========= //
 
-// setInterval(myCallback, 1000, 'Death');
+// const intervalID = setInterval(myCallback, 1000, 'Hello');
 
 // function myCallback(a) {
 // 	console.log(a, Date.now());
 // }
 
-let intervalId;
+let intervalID;
 
 function startChange() {
-	if (!intervalId) {
-		intervalId = setInterval(changeBackground, 1000);
+	if (!intervalID) {
+		intervalID = setInterval(changeRandomColor, 1000);
 	}
 }
 
-function changeBackground() {
-	if (document.body.style.backgroundColor !== 'black') {
-		document.body.style.backgroundColor = 'black';
-		document.body.style.color = 'white';
-	} else {
-		document.body.style.backgroundColor = 'white';
-		document.body.style.color = 'black';
-	}
+// function changeColor() {
+// 	if (document.body.style.backgroundColor !== 'black') {
+// 		document.body.style.backgroundColor = 'black';
+// 		document.body.style.color = 'white';
+// 	} else {
+// 		document.body.style.backgroundColor = 'white';
+// 		document.body.style.color = 'black';
+// 	}
+// }
+
+function changeRandomColor() {
+	const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+	document.body.style.backgroundColor = `#${randomColor}`;
+	console.log(randomColor);
 }
 
 function stopChange() {
-	clearInterval(intervalId);
+	clearInterval(intervalID);
+	console.log('Interval stopped');
 }
 
 document.querySelector('#start-color-change').addEventListener('click', startChange);
--- 03-callbacks/index.js
+++ 03-callbacks/index.js
@@ -1,38 +1,5 @@
 // ==================== 05 Callbacks ======================= //
 
-// const button = document.querySelector('button');
-
-// function toggle(e) {
-// 	e.target.classList.toggle('danger');
-// }
-
-// button.addEventListener('click', toggle);
-
-// const posts = [
-// 	{ title: 'Post One', body: 'Post One Body' },
-// 	{ title: 'Post Two', body: 'Post Two Body' },
-// 	{ title: 'Post Three', body: 'Post Three Body' },
-// ];
-
-// function createPost(post, cb) {
-// 	setTimeout(() => {
-// 		posts.push(post);
-// 		cb();
-// 	}, 2000);
-// }
-
-// function getPosts() {
-// 	setTimeout(() => {
-// 		posts.forEach((post) => {
-// 			const div = document.createElement('div');
-// 			div.innerHTML = `<strong>${post.title}</strong> - ${post.body}`;
-// 			document.querySelector('.posts').appendChild(div);
-// 		});
-// 	}, 1000);
-// }
-
-// createPost({ title: 'Post Four', body: 'Post Four Body' }), createPost;
-
 // function toggle(e) {
 // 	e.target.classList.toggle('danger');
 // }
@@ -40,31 +7,27 @@
 // document.querySelector('button').addEventListener('click', toggle);
 
 const posts = [
-	{ title: 'Post 1', body: 'Post 1 body' },
-	{ title: 'Post 2', body: 'Post 2 body' },
-	{ title: 'Post 3', body: 'Post 3 body' },
-	{ title: 'Post 4', body: 'Post 4 body' },
-	{ title: 'Post 5', body: 'Post 5 body' },
-	{ title: 'Post 6', body: 'Post 6 body' },
+	{ title: 'Post One', body: 'This is post one' },
+	{ title: 'Post Two', body: 'This is post two' },
 ];
 
-function createPost(post, cb) {
-	setTimeout(() => {
-		posts.push(post);
-		cb();
-	}, 2000);
-}
+const createPost = (post) => {
+	return new Promise((resolve, reject) => {
+		setTimeout(() => {
+			posts.push(post);
+			resolve();
+		}, 2000);
+	});
+};
 
-function getPosts() {
+const getPosts = () => {
 	setTimeout(() => {
 		posts.forEach((post) => {
 			const div = document.createElement('div');
-			div.innerHTML = `<strong>${post.title}</strong> - ${post.body}`;
-			document.body.appendChild(div);
+			div.innerHTML = `<strong> ${post.title} </strong> - ${post.body}`;
+			document.querySelector('.posts').appendChild(div);
 		});
 	}, 1000);
-}
+};
 
-createPost({ title: 'Post 7', body: 'Post 7 body' }, getPosts);
-
-getPosts();
+createPost({ title: 'Post Three', body: 'This is post three' }).then(getPosts);
