4ab0edeb5eda3bd2ce6b992aed0b80488e71500b
Radosław Siek <Borderlandsmaniak@gmail.com>
2023-04-05 15:09:10 +0200

    05.04

--- 05-AJAX-&-XHR/index.js
+++ 05-AJAX-&-XHR/index.js
@@ -1,16 +1,17 @@
+// ==================== 05 AJAX & XHR ==================== //
+
 const xhr = new XMLHttpRequest();
 
-// xhr.open('GET', './movies.json');
-xhr.open('GET', 'https://api.github.com/users/Radexman/repos');
+xhr.open('GET', './movies.json');
 
 xhr.onreadystatechange = function () {
-	if (this.readyState === 4 && this.status === 200) {
+	if (this.status === 200 && this.readyState === 4) {
 		// console.log(JSON.parse(this.responseText));
 		const data = JSON.parse(this.responseText);
 
-		data.forEach((repo) => {
+		data.forEach((movie) => {
 			const li = document.createElement('li');
-			li.innerHTML = `<strong>${repo.name} - ${repo.description}</strong>`;
+			li.innerHTML = `<strong> ${movie.title}  </strong> - ${movie.year}`;
 			document.querySelector('.results').appendChild(li);
 		});
 	}
