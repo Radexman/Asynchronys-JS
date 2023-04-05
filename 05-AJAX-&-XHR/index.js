6d2577e1a4bfb801fa6cf213f3c82b9e58920878
Radosław Siek <Borderlandsmaniak@gmail.com>
2023-04-05 21:11:28 +0200

    05.04

--- 05-AJAX-&-XHR/index.js
+++ 05-AJAX-&-XHR/index.js
@@ -1,20 +1,25 @@
 // ==================== 05 AJAX & XHR ==================== //
 
-const xhr = new XMLHttpRequest();
+const showPosts = () => {
+	setTimeout(() => {
+		const xhr = new XMLHttpRequest();
 
-xhr.open('GET', './movies.json');
+		xhr.open('GET', 'movies.json');
 
-xhr.onreadystatechange = function () {
-	if (this.status === 200 && this.readyState === 4) {
-		// console.log(JSON.parse(this.responseText));
-		const data = JSON.parse(this.responseText);
+		xhr.onreadystatechange = function () {
+			if (this.readyState === 4 && this.status === 200) {
+				const data = JSON.parse(this.responseText);
 
-		data.forEach((movie) => {
-			const li = document.createElement('li');
-			li.innerHTML = `<strong> ${movie.title}  </strong> - ${movie.year}`;
-			document.querySelector('.results').appendChild(li);
-		});
-	}
+				data.forEach((movie) => {
+					const div = document.createElement('div');
+					div.innerHTML = `<strong> ${movie.title} </strong> - ${movie.year}`;
+					document.querySelector('.results').appendChild(div);
+				});
+			}
+		};
+
+		xhr.send();
+	}, 2000);
 };
 
-xhr.send();
+window.addEventListener('DOMContentLoaded', showPosts);
