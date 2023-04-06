6257c2f6f3f8b158fc6b2ae79b6875d226908ef0
Radosław Siek <Borderlandsmaniak@gmail.com>
2023-04-06 16:54:07 +0200

    06.04

--- 08-promises/index.js
+++ 08-promises/index.js
@@ -2,16 +2,30 @@
 
 // Create a promise
 const promise = new Promise((resolve, reject) => {
+	// Do some async task
+	setTimeout(() => {
+		console.log('Async task complete');
+		resolve();
+	}, 1000);
+});
+
+promise.then(() => {
+	console.log('Promise consumed');
+});
+
+const getUser = new Promise((resolve, reject) => {
 	setTimeout(() => {
 		let error = false;
 
 		if (!error) {
-			resolve({ name: 'Jonh', age: 30 });
+			resolve({ name: 'Emilia', age: 20 });
 		} else {
-			reject('Error: Something went wrong');
+			reject('Error: something went wrong');
 		}
 	}, 1000);
-})
+});
+
+getUser
 	.then((user) => {
 		console.log(user);
 	})
@@ -19,7 +33,7 @@ const promise = new Promise((resolve, reject) => {
 		console.log(error);
 	})
 	.finally(() => {
-		console.log('The promise has ben resolved or rejected');
+		console.log('The promise has been resolved or rejected');
 	});
 
 console.log('Hello from global scope');
